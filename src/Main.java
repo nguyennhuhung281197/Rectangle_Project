bac0d4293448eb3bfe000161e08cc1a328cec165
nguyennhuhung281197 <nhuhung281197@gmail.com>
2023-03-22 17:09:32 +0700

    Rectanle_Projeck

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        float width;
+        float height;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Width");
+        width = scanner.nextFloat();
+
+        System.out.println("Enter height");
+        height = scanner.nextFloat();
+
+        float area = width * height;
+        System.out.println("Area = " + area);
+    }
+}
